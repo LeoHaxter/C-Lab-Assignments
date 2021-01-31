a28939fb3e175ae6d31591fb85ca1d6b1a6b37c2
LeoHaxter <72493902+LeoHaxter@users.noreply.github.com>
2021-01-31 13:16:57 +0530

    Add files via upload

---
+++ Class 2/a3.c
@@ -0,0 +1,10 @@
+#include<stdio.h> 
+int main()
+{
+     
+    printf("| /\t---\t---\t---");
+     printf("\n|/\t | \t | \t |"); 
+    printf("\n|\\t | \t | \t |");
+     printf("\n| \\t---\t---\t |");  
+   return 0;
+}
---
+++ Class 2/as2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	//print name, rollno., marks of 3 students
+	printf("NAME\tROLL NO.\tMARKS");
+	printf("\n----\t--------\t-----");
+	printf("\nABC\t2031\t\t34");
+	printf("\nDEF\t2092\t\t78");
+	printf("\nXYZ\t2013\t\t87");
+	return 0;
+}
